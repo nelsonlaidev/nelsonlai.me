104f6f1769a92cbe0168eafa0fe4e95f59973380
Nelson Lai <me@nelsonlai.me>
2025-08-31 11:09:49 +0800

    refactor: no need to specific tsconfigRootDir manually

--- packages/db/eslint.config.ts
+++ packages/db/eslint.config.ts
@@ -1,5 +1,3 @@
 import { defineConfig } from '@nelsonlaidev/eslint-config'
 
-export default defineConfig({
-  tsconfigRootDir: import.meta.dirname
-})
+export default defineConfig()
--- packages/emails/eslint.config.ts
+++ packages/emails/eslint.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from '@nelsonlaidev/eslint-config'
 
 export default defineConfig({
-  tsconfigRootDir: import.meta.dirname,
   tailwindEntryPoint: './src/styles.css'
 })
