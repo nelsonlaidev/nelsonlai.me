c4ac31f8fe3b36e85cf4eace2c40d8fd0293bf45
Nelson Lai <me@nelsonlai.dev>
2025-09-08 20:04:06 +0800

    refactor: localize og image for blog posts

--- apps/web/src/app/[locale]/(main)/blog/[slug]/og-image.png/route.tsx
+++ apps/web/src/app/[locale]/(main)/blog/[slug]/og-image.png/route.tsx
@@ -4,19 +4,15 @@ import { ImageResponse } from 'next/og'
 import { NextResponse } from 'next/server'
 
 import OGImage from '@/components/og-image'
-import { getMediumFont, getRegularFont, getSemiBoldFont } from '@/lib/fonts'
+import { getOGImageFonts } from '@/lib/fonts'
 
 export const GET = async (_request: Request, props: RouteContext<'/[locale]/blog/[slug]/og-image.png'>) => {
   const { params } = props
-  const { slug } = await params
+  const { slug, locale } = await params
 
   try {
-    const [regularFontData, mediumFontData, semiBoldFontData] = await Promise.all([
-      getRegularFont(),
-      getMediumFont(),
-      getSemiBoldFont()
-    ])
-    const post = allPosts.find((p) => p.slug === slug)
+    const ogImageFonts = await getOGImageFonts()
+    const post = allPosts.find((p) => p.slug === slug && p.locale === locale)
 
     if (!post) {
       return NextResponse.json({ error: 'Post not found' }, { status: 404 })
@@ -25,26 +21,7 @@ export const GET = async (_request: Request, props: RouteContext<'/[locale]/blog
     return new ImageResponse(<OGImage title={post.title} url='/blog' />, {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: 'Geist Sans',
-          data: regularFontData,
-          style: 'normal',
-          weight: 400
-        },
-        {
-          name: 'Geist Sans',
-          data: mediumFontData,
-          style: 'normal',
-          weight: 500
-        },
-        {
-          name: 'Geist Sans',
-          data: semiBoldFontData,
-          style: 'normal',
-          weight: 600
-        }
-      ]
+      fonts: ogImageFonts
     })
   } catch (error) {
     return NextResponse.json(
