a412bf78d65ba8f4a41061c40aa79ec105162876
Nelson Lai <me@nelsonlai.dev>
2025-09-08 17:00:00 +0800

    fix(web): use fetch for getting fonts since we're using monorepo

--- apps/web/src/lib/fonts.ts
+++ apps/web/src/lib/fonts.ts
@@ -1,24 +1,24 @@
-import fs from 'node:fs/promises'
-
 import { cache } from 'react'
 
+import { getBaseUrl } from '@/utils/get-base-url'
+
 export const getRegularFont = cache(async () => {
-  const response = await fs.readFile('public/fonts/Geist-Regular.otf')
-  const font = Uint8Array.from(response).buffer
+  const response = await fetch(`${getBaseUrl()}/fonts/Geist-Regular.otf`)
+  const font = await response.arrayBuffer()
 
   return font
 })
 
 export const getMediumFont = cache(async () => {
-  const response = await fs.readFile('public/fonts/Geist-Medium.otf')
-  const font = Uint8Array.from(response).buffer
+  const response = await fetch(`${getBaseUrl()}/fonts/Geist-Medium.otf`)
+  const font = await response.arrayBuffer()
 
   return font
 })
 
 export const getSemiBoldFont = cache(async () => {
-  const response = await fs.readFile('public/fonts/Geist-SemiBold.otf')
-  const font = Uint8Array.from(response).buffer
+  const response = await fetch(`${getBaseUrl()}/fonts/Geist-SemiBold.otf`)
+  const font = await response.arrayBuffer()
 
   return font
 })
