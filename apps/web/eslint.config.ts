870689114d460299f4bc10c3275fea20b84f5a90
Nelson Lai <me@nelsonlai.dev>
2025-09-16 22:45:03 +0800

    chore(web): update eslint config to use pre-defined glob

--- apps/web/eslint.config.ts
+++ apps/web/eslint.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from '@nelsonlaidev/eslint-config'
+import { defineConfig, GLOB_SRC_EXT } from '@nelsonlaidev/eslint-config'
 
 export default defineConfig({
   tailwindEntryPoint: './src/styles/globals.css',
-  playwrightGlob: './src/e2e/**/*.test.ts',
-  vitestGlob: './src/tests/**/*.test.{ts,tsx}',
+  playwrightGlob: `./src/e2e/**/*.test.${GLOB_SRC_EXT}`,
+  vitestGlob: `./src/tests/**/*.test.${GLOB_SRC_EXT}`,
   overrides: {
     tailwindcss: {
       'better-tailwindcss/no-unregistered-classes': ['error', { ignore: ['not-prose', 'shiki'] }]
