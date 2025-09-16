9a28a8f417cfdfe3f3e4e31e0cf5e8116824f66a
Nelson Lai <me@nelsonlai.dev>
2025-09-16 22:40:54 +0800

    chore(web): enable playwright eslint plugin

--- apps/web/eslint.config.ts
+++ apps/web/eslint.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from '@nelsonlaidev/eslint-config'
 
 export default defineConfig({
   tailwindEntryPoint: './src/styles/globals.css',
+  playwrightGlob: './src/e2e/**/*.test.ts',
+  vitestGlob: './src/tests/**/*.test.{ts,tsx}',
   overrides: {
     tailwindcss: {
       'better-tailwindcss/no-unregistered-classes': ['error', { ignore: ['not-prose', 'shiki'] }]
